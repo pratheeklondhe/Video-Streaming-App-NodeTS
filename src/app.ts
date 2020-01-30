9020e73dcf0f652952466074300655b7241870ff
PRATHEEK <pratheeklondhe2014@gmail.com>
2020-01-30 22:35:28 +0530

    genre-model validation added

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { errBuilder } from './custom-utilities/error-service';
 import { userCreationRouter }  from './user-creation/user-creation';
 import { userAuthenticationRouter } from './authenticate/user-authentication';
 import { authenticateToken } from './middleware/auth-token';
+import { genreModel, genreSchema } from './genre/genre-model';
 
 app.use(bodyParser.json());
 app.use(setCors);
@@ -20,6 +21,17 @@ app.use('/api/userauthentication' , userAuthenticationRouter);
 app.get('/', (req: Request, res: Response) => {
     res.status(400).send(errBuilder('dsgaf', 'Appconp'));
 });
+
+app.post('/genre' , (req, res) => {
+    try{
+        // genreSchema.()
+        (new genreModel(req.body) as any).validateRequirements();
+        console.log(req.body);
+        new genreModel(req.body).save();
+    } catch(e) {
+        throw e;
+    }
+})
 
 
 
--- src/genre/genre-model.ts
+++ src/genre/genre-model.ts
@@ -2,20 +2,39 @@ import { Mongo } from "../config/appconfig";
 import { Languages } from "./entity/genre-language";
 import { Categories } from "./entity/genre-categories";
 
-const s = new Mongo.Schema({
-    genreId: { type: String, minlength: 3, required: true, unique: true },
-    title: { type: String, minlength: 3, required: true },
-    description: { type: String, minlength: 10, required: true},
-    language: { type: Languages, minlength: 2, required: true},
-    category: { type: [Categories], required: true },
-    year: { type: String, required: true },
-    displayImg: { type: String, required: true },
-    screenshots: { type: [String] },
+export var genreSchema = new Mongo.Schema({
+    // genreId: { type: String, minlength: 3, required: true, unique: true },
+    // title: { type: String, minlength: 3, required: true },
+    // description: { type: String, minlength: 10, required: true},
+    // language: { type: Languages, minlength: 2, required: true},
+    // category: { type: [Categories], required: true },
+    // year: { type: String, required: true },
+    // displayImg: { type: String, required: true },
+    // screenshots: { type: [String] },
     isSeries: { type: Boolean, default: false, required: true },
-    cast: { type: String, required: true },
-    crew: { type: String, required: true },
-    seriesId: { type: String, required: function() { return this.isSeries }},
-    seasonNo: { type: Number, required: function() { return this.isSeries }},
-    episodeNo: { type: Number, required: function() { return this.isSeries }}
-})
+    // cast: { type: String, required: true },
+    // crew: { type: String, required: true },
+    seriesId: { type: String },
+    seasonNo: { type: Number },
+    episodeNo: { type: Number }
+});
+
+genreSchema.methods.validateRequirements = function() {
+    console.log(`Tiggered ${this.isSeries} ${this.seriesId} ${this.seasonNo}`);
+    if(this.isSeries) {
+        if(this.seriesId == null || this.seriesId == undefined ) {
+            throw 'Series Id is required';
+        }
+        if(this.seasonNo == null || this.seasonNo == undefined ) {
+            throw 'Season Number is required';
+        }
+        if(this.episodeNo == null || this.episodeNo == undefined ) {
+            throw 'Episode Number is required';
+        }
+    }
+};
+
+// genreSchema.vali
+
+export let genreModel = Mongo.model('vsnodeapp_genre', genreSchema, 'vsnodeapp_genre');
 
