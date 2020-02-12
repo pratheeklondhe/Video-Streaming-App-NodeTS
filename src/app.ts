e666ccd822634b07d95a7011d51469bd9d91d695
PRATHEEK <pratheeklondhe2014@gmail.com>
2020-02-12 21:45:01 +0530

    added video fs

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,21 @@ import bodyParser from 'body-parser';
 import { setCors } from './middleware/CORS';
 import { Request, Response } from 'express';
 import { errBuilder } from './custom-utilities/error-service';
-
 import { genreModel } from './genre/models/genre-model';
 import { initializeRouters } from './custom-utilities/router-initialize';
+
+import fs  from 'fs';
 
 app.use(bodyParser.json());
 app.use(setCors);
 app.use(logger);
 initializeRouters(app);
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(400).send(errBuilder('dsgaf', 'Appconp'));
+app.get('/api/', (req: Request, res: Response) => {
+    res.writeHead(200, {'Content-Type' : 'video/mp4'});
+    const rs = fs.createReadStream('/home/pratheek/Desktop/Pratheek/Video-Streaming-App-NodeTS-master/assets/file_example_MP4_1920_18MG.mp4');
+    rs.pipe(res);
+    // res.status(400).send(errBuilder('dsgaf', 'Appconp'));
 });
 
 app.post('/genre' , (req) => {
